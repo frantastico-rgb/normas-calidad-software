a9cc61aadd6eb5c988ffdeafe3838c5754771d25
frantastico-rgb <francisco_valenzuela@soy.sena.edu.co>
2025-12-04 18:24:52 -0500

    vite.config.js base actualizado

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-    // Use the repo subpath only for production builds (GitHub Pages).
-    base: command === 'build' ? '/normas-calidad-software/' : '/',
+export default defineConfig({
+    base: './',
     plugins: [react()],
-}))
+
+})
